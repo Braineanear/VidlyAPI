9d86a5775600d230e43b4428299f397c227fdfa6
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-08 11:05:18 +0200

    :art: Added some API ENDPOINTS & Improved the userRoutes Structure

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const APIFeatures = require('../utils/apiFeatures');
+const User = require('../models/userModel');
 
 const router = express.Router();
 
@@ -11,8 +14,33 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 //Protect all routes after this middleware
 router.use(authController.protect);
 
+router
+  .route('/personal')
+  .get(userController.getMe, userController.getUser)
+  .patch(userController.updateMe)
+  .delete(userController.deleteMe);
+
+router
+  .route('/personal/avatar')
+  .patch(userController.uploadAvatar, userController.resizeImages)
+  .delete(userController.deleteAvatar);
+
 router.get('/confirmEmail', authController.confirmEmail);
-router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/logout', authController.logout);
+router.get('/:id/avatar', userController.getUserAvatar);
+router.patch('/updatePassword', authController.updatePassword);
+
+router.use(authController.restrictTo('admin'));
+
+router
+  .route('/')
+  .get(APIFeatures(User), userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
