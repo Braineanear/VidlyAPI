ddf5e45372ab3cf8b7292aa4e1aa6a9ad2e2f2d4
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-08 21:02:07 +0200

    :bulb: Adding some comments to the apiFeatues

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -5,32 +5,41 @@ const advancedResults = (model, populate) =>
   catchAsync(async (req, res, next) => {
     let query;
 
+    // Copying the req.query data and store it
     const reqQuery = { ...req.query };
 
+    // Fields to exclude
     const removeFields = ['select', 'sort', 'page', 'limit'];
 
+    // Loop over  removeFields and delete them from reqQuery
     removeFields.forEach((params) => delete reqQuery[params]);
 
+    // Create query string
     let queryStr = JSON.stringify(reqQuery);
 
+    // Create operators ($gt / $gte / etc...)
     queryStr = queryStr.replace(
       /\b(gt|gte|lt|lte|in)\b/g,
       (match) => `$${match}`
     );
 
+    // Finding resource
     query = model.find(JSON.parse(queryStr)).lean();
 
     if (!query) {
       return next(new AppError('No Data Found', 400));
     }
 
+    // Selecting
     if (req.query.select) {
       const fields = req.query.select.split(',').join(' ');
       query = query.select(fields);
     } else {
+      // Excluding __v which mongo create on each record to use it by using select and putting dash -
       query = query.select('-__v');
     }
 
+    // Sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
@@ -38,6 +47,7 @@ const advancedResults = (model, populate) =>
       query = query.sort('-createdAt');
     }
 
+    // Pagination
     const page = req.query.page * 1 || 1;
     const limit = req.query.limit * 1 || 100;
     const skip = (page - 1) * limit;
@@ -48,6 +58,7 @@ const advancedResults = (model, populate) =>
       query = query.populate(populate);
     }
 
+    // Excluding query
     query = await query;
 
     res.advancedResults = {
