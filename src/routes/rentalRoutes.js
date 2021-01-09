887de0ddc494a84ffee9b379617e307074080269
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-09 23:23:21 +0200

    :zap: Convert From CommonJS to ESModule Import/Export

--- src/routes/rentalRoutes.js
+++ src/routes/rentalRoutes.js
@@ -1,31 +1,30 @@
-const express = require('express');
-const rentalController = require('../controllers/rentalController');
-const authController = require('../controllers/authController');
-const APIFeatures = require('../utils/apiFeatures');
-const Rental = require('../models/rentalModel');
+import { Router } from 'express';
+import {
+  getAllRentals,
+  createRental,
+  getRental,
+  updateRental,
+  deleteRental
+} from '../controllers/rentalController';
+import { protect } from '../controllers/authController';
+import APIFeatures from '../utils/apiFeatures';
+import Rental from '../models/rentalModel';
 
-const router = express.Router();
+const router = Router();
 
 //===========================Public Routes===================================//
 //ANY ONE CAN ACCESS THESE ROUTES
 
 //Get All Rentals Route
-router.get('/', APIFeatures(Rental), rentalController.getAllRentals);
+router.get('/', APIFeatures(Rental), getAllRentals);
 
 //====================User's Routes / Private Routes=========================//
 // Protect all routes after this middleware
 // ONLY LOGGED IN USERS CAN ACCESS THESE ROUTES
-router.use(authController.protect);
+router.use(protect);
 
-router
-  .route('/')
-  .get(APIFeatures(Rental), rentalController.getAllRentals)
-  .post(rentalController.createRental);
+router.route('/').get(APIFeatures(Rental), getAllRentals).post(createRental);
 
-router
-  .route('/:id')
-  .get(rentalController.getRental)
-  .patch(rentalController.updateRental)
-  .delete(rentalController.deleteRental);
+router.route('/:id').get(getRental).patch(updateRental).delete(deleteRental);
 
-module.exports = router;
+export default router;
