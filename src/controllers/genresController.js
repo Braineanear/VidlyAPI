e02b19b986df0c81df59a365bc1bb66818f48693
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-09 23:22:23 +0200

    :zap: Convert From CommonJS to ESModule Import/Export

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -1,30 +1,30 @@
-const catchAsync = require('../utils/catchAsync');
-const Genre = require('../models/genreModel');
-const factory = require('./handlerFactory');
+import catchAsync from '../utils/catchAsync.js';
+import Genre from '../models/genreModel.js';
+import { getOne, createOne, updateOne, deleteOne } from './handlerFactory.js';
 
 // @desc    Get All Genres Data
 // @route   GET /api/v1/genres
 // @access  Private ==> Current User
-exports.getAllGenres = catchAsync(async (req, res, next) => {
+export const getAllGenres = catchAsync(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get Specific Genre's Data
 // @route   GET /api/v1/genres/:id
 // @access  Private ==> Current User
-exports.getGenre = factory.getOne(Genre);
+export const getGenre = getOne(Genre);
 
 // @desc    Create New Genre
 // @route   POST /api/v1/genres
 // @access  Private ==> Current User
-exports.createGenre = factory.createOne(Genre);
+export const createGenre = createOne(Genre);
 
 // @desc    Updating Genre's Data
 // @route   PATCH /api/v1/genres/:id
 // @access  Private ==> Current User
-exports.updateGenre = factory.updateOne(Genre);
+export const updateGenre = updateOne(Genre);
 
 // @desc    Deleting Genre's Data
 // @route   DELETE /api/v1/genres/:id
 // @access  Private ==> Current User
-exports.deleteGenre = factory.deleteOne(Genre);
+export const deleteGenre = deleteOne(Genre);
