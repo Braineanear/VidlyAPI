ec5c2b39457ebe3de597ef5c14b2456371e17a5a
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-09 23:42:31 +0200

    :zap: Convert From CommonJS to ESModule Import/Export

--- src/utils/imageProcess.js
+++ src/utils/imageProcess.js
@@ -1,33 +1,31 @@
-const fs = require('fs');
-const dotenv = require('dotenv');
-const sharp = require('sharp');
-const path = require('path');
-const catchAsync = require('./catchAsync');
+import { access, mkdirSync, unlink } from 'fs';
+import { config } from 'dotenv';
+import sharp from 'sharp';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config({ path: 'config.env' });
+import catchAsync from './catchAsync.js';
 
-// @desc    Uploading image to the database and the server
-// @route   NONE
-// @access  Private ==> Admin/User
-exports.imageUpload = catchAsync(async (req) => {
-  // Get the path of the foleder where the image is going to be saved
-  const filePath = path.resolve(
-    `${__dirname}/../${process.env.FILE_UPLOAD_PATH}`
-  );
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+config({ path: 'config.env' });
+
+export const imageUpload = catchAsync(async (req) => {
+  // Get the path of the folder where the image is going to be saved
+  const folderPath = path.resolve(path.join(__dirname, '..', process.env.FILE_UPLOAD_PATH));
 
   // Access the folder where images is going to be saved
-  fs.access(filePath, (err) => {
+  access(folderPath, (err) => {
     // If the folder doesn't not exist, create new one
     if (err) {
-      fs.mkdirSync(filePath);
+      mkdirSync(folderPath);
     }
   });
 
   // Get the uploaded image name and replace the spaces in the name with dash
-  const formatedName = req.file.originalname.split(' ').join('-');
-  const fileName = `user-${Date.now()}-${formatedName}`;
-
-  // Resize the image and change it's type to jpge then save it into it's folder in the server
+  const fileName = `user-${Date.now()}-${req.file.originalname.split(' ').join('-')}`;
+  const filePath = path.resolve(path.join(folderPath, fileName));
+  // Resize the image and change it's type to jpeg then save it into it's folder in the server
   await sharp(req.file.buffer)
     .resize({
       fit: sharp.fit.contain,
@@ -35,21 +33,19 @@ exports.imageUpload = catchAsync(async (req) => {
     })
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`${filePath}/${fileName}`);
+    .toFile(filePath);
 
   // Return the image path on the server
-  return `${filePath}/${fileName}`;
+  return filePath;
 });
 
-// @desc    Deleting image from the database and the server
-// @route   NONE
-// @access  Private ==> Admin/User
-exports.imageDelete = catchAsync(async (req, res, next) => {
+export const imageDelete = (req, res, next) => {
   // Checking if the avatar field in the database equal NULL
   // If the avatar field == NULL then it will return true to tell you that you cannot delete something that does not exist
   if (req.avatar == null) return true;
 
   // If the avatar field value not equal to NULL
   // Then it will start deleting the image from the server storage
-  fs.unlink(req.avatar, (err) => err);
-});
+  unlink(req.avatar, (err) => err);
+  return false;
+};
