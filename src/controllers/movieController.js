45925d3baf17715774d6a694ebeb3de3a1460d86
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-09 23:20:48 +0200

    :zap: Convert From CommonJS to ESModule Import/Export

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,30 +1,30 @@
-const catchAsync = require('../utils/catchAsync');
-const Movie = require('../models/movieModel');
-const factory = require('./handlerFactory');
+import catchAsync from '../utils/catchAsync.js';
+import Movie from '../models/movieModel.js';
+import { getOne, createOne, updateOne, deleteOne } from './handlerFactory.js';
 
-// @desc    Get All Modvies Data
+// @desc    Get All Movie's Data
 // @route   GET /api/v1/movies
 // @access  Private ==> Current User
-exports.getAllMovies = catchAsync(async (req, res, next) => {
+export const getAllMovies = catchAsync(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get Specific Movie's Data
 // @route   GET /api/v1/movies/:id
 // @access  Private ==> Current User
-exports.getMovie = factory.getOne(Movie);
+export const getMovie = getOne(Movie);
 
 // @desc    Create New Movie
 // @route   POST /api/v1/movies
 // @access  Private ==> Current User
-exports.createMovie = factory.createOne(Movie);
+export const createMovie = createOne(Movie);
 
 // @desc    Updating Movie's Data
 // @route   PATCH /api/v1/movies/:id
 // @access  Private ==> Current User
-exports.updateMovie = factory.updateOne(Movie);
+export const updateMovie = updateOne(Movie);
 
 // @desc    Deleting Movie's data
 // @route   DELETE /api/v1/movies/:id
 // @access  Private ==> Current User
-exports.deleteMovie = factory.deleteOne(Movie);
+export const deleteMovie = deleteOne(Movie);
