6aa09f7ddd9f9026eb1fe2a645f711a3e4383789
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-09 23:19:49 +0200

    :zap: Convert From CommonJS to ESModule Import/Export

--- src/models/genreModel.js
+++ src/models/genreModel.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const genreSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please enter the genre name!'],
     minlength: 5,
-    maxlength: 50
+    maxlength: 50,
+    unique: true
   }
 });
 
-module.exports = mongoose.model('Genre', genreSchema);
+export default mongoose.model('Genre', genreSchema);
