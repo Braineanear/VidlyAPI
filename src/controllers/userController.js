ca27faff61c84d58c6801d30565d82c9c220b701
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-11 06:18:01 +0200

    :art: Restructuring The User Controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,7 @@
-import multer from 'multer';
 import catchAsync from '../utils/catchAsync.js';
 import AppError from '../utils/appError.js';
 import User from '../models/userModel.js';
-import {
-  getOne,
-  createOne,
-  updateOne,
-  deleteOne as _deleteOne
-} from './handlerFactory.js';
-import { imageUpload, imageDelete } from '../utils/imageProcess.js';
+import { getOne, createOne, updateOne, deleteOne } from './handlerFactory.js';
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -17,117 +10,6 @@ const filterObj = (obj, ...allowedFields) => {
   });
   return newObj;
 };
-
-// @desc    Upload User Photo
-// @route   PATCH /api/v1/users/personal/avatar
-// @access  Private ==> Current User
-export const uploadAvatar = multer({
-  limits: {
-    fileSize: 1000000
-  },
-  multerFilter(request, file, cb) {
-    // Accept images only
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-      request.fileValidationError = 'Only image files are allowed!';
-      return cb(
-        new AppError('Not an image! Please upload only images.'),
-        false
-      );
-    }
-
-    cb(null, true);
-  }
-}).single('photo');
-
-// @desc    Resizing User Photo
-// @route   PATCH /api/v1/users/personal/avatar
-// @access  Private ==> Current User
-export const resizeImages = catchAsync(async (req, res) => {
-  // Check first if there is an existing avatar on the database
-  // If that's true, then call imageDelete function to delete them
-  const user = await User.findById(req.user.id).lean();
-  if (user.avatar != null) {
-    imageDelete(user);
-  }
-  // Getting the image path after resizing it & Uploading it to the database
-  const image = await imageUpload(req);
-
-  // Updating The User's Avatar Field by assigning the Avatar's Path to it.
-  await User.findByIdAndUpdate(
-    req.user.id,
-    { avatar: image },
-    {
-      new: true,
-      runValidators: true
-    }
-  );
-
-  // Process Done Successfully
-  res.status(200).json({
-    status: 'success',
-    path: image
-  });
-});
-
-// @desc    Deleting User's Avatar
-// @route   DELETE /api/v1/users/personal/avatar
-// @access  Private ==> Current User
-export const deleteAvatar = catchAsync(async (req, res, next) => {
-  // Getting User's Data From the Database
-  const user = await User.findById(req.user.id).lean();
-
-  // Calling The imageDelete Function to Delete the Avatar by sending to it User's Data
-  const Delete = imageDelete(user);
-
-  // If the imageData Returned false, then thi means that the function found the avatar's field and it found that this field doesn't contain any null data (Which means it contains the actual path of the avatar)
-  // The imageDelete Function will go then to the server storage where the avatar locates and then delete that avatar
-  if (!Delete) {
-    // Updating The User's Data by setting the avatar's field to undefined
-    await User.findByIdAndUpdate(
-      req.user.id,
-      { avatar: undefined },
-      {
-        new: true,
-        runValidators: true
-      }
-    );
-
-    // Process Done Successfully
-    res.status(200).json({
-      status: 'success',
-      message: 'Avatar Successfully Deleted'
-    });
-  } else {
-    // If the imageDelete function found that the avatar's field contain null then it will return true, which means you can't delete something that doesn't exist.
-    res.status(400).json({
-      message: 'There is no avatar to delete'
-    });
-  }
-});
-
-// @desc    Get User's Avatar
-// @route   GET /api/v1/users/:id/avatar
-// @access  Private ==> Current User
-export const getUserAvatar = catchAsync(async (req, res, next) => {
-  // Get The User From The Database
-  const user = await User.findById(req.params.id);
-
-  // Check if the user exists and the user's Avatar Field not equal to NULL
-  if (!user || user.avatar == null) {
-    return next(
-      new AppError(
-        'Maybe the user does not exist or the avatar was deleted',
-        400
-      )
-    );
-  }
-
-  // Response with Success
-  res.status(200).json({
-    status: 'success',
-    path: user.avatar
-  });
-});
 
 // @desc    Get All Users
 // @route   GET /api/v1/users
@@ -154,7 +36,7 @@ export const updateUser = updateOne(User);
 // @desc    Delete Single User
 // @route   DELETE /api/v1/users/:id
 // @access  Private ==> Admin
-export const deleteUser = _deleteOne(User);
+export const deleteUser = deleteOne(User);
 
 // @desc    Get Current User Data
 // @route   GET /api/v1/users/personal
