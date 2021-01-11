40afb463e7bf52c24e4a02d1ff064db413323380
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-11 06:17:18 +0200

    :sparkles: Adding Lookup to The Model

--- src/models/rentalModel.js
+++ src/models/rentalModel.js
@@ -26,6 +26,13 @@ const rentalSchema = new mongoose.Schema({
   }
 });
 
+rentalSchema.statics.lookup = function (customerId, movieId) {
+  return this.findOne({
+    'customer._id': customerId,
+    'movie._id': movieId
+  });
+};
+
 rentalSchema.methods.return = function () {
   this.dateReturned = new Date();
 
