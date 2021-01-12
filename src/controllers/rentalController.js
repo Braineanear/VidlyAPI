a9046296c4b82c9787fd7972e8a6615ff6d023d8
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-01-12 02:29:51 +0200

    :zap: Editing the getRental Controller so it does not use the handlerFactory because of a problem with populate

--- src/controllers/rentalController.js
+++ src/controllers/rentalController.js
@@ -1,6 +1,7 @@
 import catchAsync from '../utils/catchAsync.js';
 import Rental from '../models/rentalModel.js';
-import { getOne, createOne, updateOne, deleteOne } from './handlerFactory.js';
+import { createOne, updateOne, deleteOne } from './handlerFactory.js';
+import AppError from '../utils/appError.js';
 
 // @desc    Get All Rental's Data
 // @route   GET /api/v1/rentals
@@ -12,7 +13,32 @@ export const getAllRentals = catchAsync(async (req, res, next) => {
 // @desc    Get Specific Rental's Data
 // @route   GET /api/v1/rentals/:id
 // @access  Private ==> Current User
-export const getRental = getOne(Rental);
+export const getRental = catchAsync(async (req, res, next) => {
+  let query = Rental.findById(req.params.id);
+  query = query.populate([
+    {
+      path: 'movie',
+      select: 'title genre year numberInStock dailyRentalRate -_id',
+      populate: {
+        path: 'genre',
+        model: 'Genre',
+        select: 'name -_id'
+      }
+    },
+    { path: 'customer', select: 'name -_id' }
+  ]);
+
+  const doc = await query;
+
+  if (!doc) {
+    return next(new AppError('No Result Found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    doc
+  });
+});
 
 // @desc    Create New Rental
 // @route   POST /api/v1/rentals
